b05015ab4b4940a216fb249aba79c300dc42539f
BoyDido <dimitritanghe@telenet.be>
2020-10-14 20:39:58 +0200

    Second Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first-app';
-}
+    title= 'Raad het getal'
+      gokgetalString: string;
+      randomNumber: number;
+      gokBoodschap: string;
+      beurten: number;
+      controle: number;
+      gokgetalNumber:number;
+      controleNummer: number
+
+      constructor(){
+      this.gokBoodschap = this.gokBoodschap;
+      this.startSpel();
+      }
+
+    startSpel(){
+        this.beurten = 10;
+        this.gokgetalString= ''; 
+        this.randomNumber = Math.floor(Math.random()*100);
+        this.gokBoodschap= '';
+        this.controle= 0;
+        this.controleNummer= 0
+    }
+
+    valideerGok(gokgetalNumber){
+            gokgetalNumber = +this.gokgetalString; 
+            this.gokBoodschap= 'Kies een ander getal';
+            if (gokgetalNumber <= -1 || gokgetalNumber > 100) {
+            return this.gokBoodschap}
+    }
+
+    nummerControle() {
+          this.controle = this.randomNumber - this.gokgetalNumber;
+          this.beurten = this.beurten - 1 ;
+        }
+
+      groterDan(gokgetalNumber, controleNummer) {
+          return gokgetalNumber < controleNummer;
+        }
+  }
+ 
